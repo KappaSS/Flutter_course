30eeac8106e17141b974a657d55b1b4ea09c0aa1
KappaSS <dani.pervann@gmail.com>
2021-03-29 20:40:56 +0200

    Step 2
    Basics understanding of dart and flutter widgets

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// Step is a Mobile/Web app that will connect you with some other people.
+// Currently state: alpha 0.0.1
+// Made by 'An Organization' with Flutter (based on dart).
+// For any question on how the app works check readme.txt file or to take a look on the source code check https://github.com/KappaSS/Flutter_course.
+
+// imports:
+import 'package:flutter/material.dart';
+
+// TODO:
+void main() {
+  // This is the main function where the whole app is constructed
+  runApp(Step());
+
+}
+
+class Step extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('CULO'),);
+  }
+}
+
